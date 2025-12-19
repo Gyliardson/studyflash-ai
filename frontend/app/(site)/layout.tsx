f26304f3d5ea801bdba2ed4f1fda3a15c45434b4
Gyliardson Keitison <gylifodaot@gmail.com>
2025-12-18 22:33:59 -0300

    Footer

--- frontend/app/(site)/layout.tsx
+++ frontend/app/(site)/layout.tsx
@@ -13,7 +13,7 @@ export default async function SiteLayout({
   return (
     <div className="flex flex-col min-h-screen bg-background">
       {/* Navbar */}
-      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
         <div className="container flex h-14 max-w-screen-2xl items-center px-4 md:px-8">
           <div className="mr-4 hidden md:flex">
             <Link className="mr-6 flex items-center space-x-2" href="/">
@@ -52,8 +52,8 @@ export default async function SiteLayout({
       {/* Footer */}
       <footer className="py-6 md:px-8 md:py-0">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-          <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-            Built by StudyFlash AI. The source code is available on GitHub.
+          <p className="mb-4">
+            © {new Date().getFullYear()} StudyFlash AI • Desenvolvido por <span className="font-semibold text-slate-700 dark:text-slate-300">Gyliardson Keitison</span>
           </p>
           <div className="flex gap-4 items-center">
              <Link href="/termos" className="text-sm font-medium hover:underline underline-offset-4 text-muted-foreground">
